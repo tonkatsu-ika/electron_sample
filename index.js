d35a84467a45c7ab596851b5321adbe81cfc0433
naoki <duvingtdeux@gmail.com>
2019-12-24 15:50:56 +0900

    Angularを入れたことに伴いindex.jsを修正

--- index.js
+++ index.js
@@ -3,7 +3,30 @@
 const electron = require('electron');
 const remote = electron.remote;
 const fileUtil = remote.require('./lib/fileUtil');
+const baseDir = process.cwd();
 
-fileUtil.fetchReadmeList( (err, matches) => {
-  if(!err) document.write(matches.join());
+var ngModule = angular.module('readUs', []);
+
+ngModule.controller('MainController', function($scope) {
+  var main = this;
+
+  main.getFile = function(file) {
+    main.fileText = fileUtil.getAsText(file.filepath);
+  };
+
+  fileUtil.fetchReadmeList(baseDir, (err, fileList) => {
+    if(!err) console.error(err);
+    $scope.$apply( () => {
+      main.fileList = fileList;
+    });
+  });
 });
+
+ngModule.directive('mdPreview', () => {
+  return function ($scope, $elem, $attrs) {
+    $scope.$watch($attrs.mdPreview, (source) => {
+      $elem.html(marked(source));
+    });
+  };
+});
+
