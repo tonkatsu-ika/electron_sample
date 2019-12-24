86b106eb7bff6335bc2d8bb13aedf9889d97163c
naoki <duvingtdeux@gmail.com>
2019-12-24 15:03:56 +0900

    main.jsとindex.jsを作成

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 const electron = require('electron');
 const app = electron.app;
-const BrowseWindow = electron.BrowseWindow;
+const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
 
-app.on('window-all-closed', function(){
 
-  if(process.platform !== 'darwin'){
+app.on('ready', () => {
+
+  mainWindow = new BrowserWindow({
+    width: 1000, 
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true
+    },
+  });
+
+  mainWindow.loadURL('index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+});
+
+
+app.on('window-all-closed', () => {
+
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 
 });
 
 
-app.on('ready', function() {
-
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + 'index.html');
-  mainWindow.on('closed', function(){
-    mainWindow = null;
-  });
-
-});
